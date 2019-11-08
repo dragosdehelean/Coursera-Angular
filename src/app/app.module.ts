0613dcdf95d2c32889a58940556f1561872f79cb
Dragos Dehelean <dragos.dehelean1@vodafone.com>
2019-11-08 16:42:28 +0200

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,16 @@ import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from './app.component';
 import "hammerjs";
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from "@angular/material/list";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatListModule,
     FlexLayoutModule
   ],
   providers: [],
