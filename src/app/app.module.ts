3fbdfe73754313a948440d1e723483bc5a2081de
Dragos Dehelean <dragos.dehelean1@vodafone.com>
2019-11-12 12:34:33 +0200

    header & footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-
 import { AppComponent } from './app.component';
 import "hammerjs";
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from "@angular/material/list";
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from "./services/dish.service";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishdetailComponent],
+  declarations: [AppComponent, MenuComponent, DishdetailComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -23,9 +25,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     FlexLayoutModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
