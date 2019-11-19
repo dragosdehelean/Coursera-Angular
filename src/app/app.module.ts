52bda41b22b961bea84d30680c7ac842d38de145
Dragos Dehelean <dragos.dehelean1@vodafone.com>
2019-11-19 16:26:28 +0200

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from "./app-routing/app-routing.module";
 import {PromotionService} from "./services/promotion.service";
 import {LeaderService} from "./services/leader.service";
+import { LoginComponent } from './login/login.component';
+import { MatDialogModule } from "@angular/material/dialog";
 
 @NgModule({
   declarations: [
@@ -30,7 +32,8 @@ import {LeaderService} from "./services/leader.service";
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -41,9 +44,11 @@ import {LeaderService} from "./services/leader.service";
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule
   ],
   providers: [DishService, PromotionService, LeaderService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [LoginComponent]
 })
 export class AppModule {}
